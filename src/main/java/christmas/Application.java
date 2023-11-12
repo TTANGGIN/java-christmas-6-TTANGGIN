7e5d8ac8636ec0b58eb68673ec4f345b979781f3
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-12 15:46:36 +0900

    feat(Application): 프로그램 동작 구현 완료

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,16 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.model.EventPlanningService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = InputView.getInstance();
+        OutputView outputView = OutputView.getInstance();
+        EventPlanningService eventPlanningService = EventPlanningService.getInstance();
+        ChristmasController controller = new ChristmasController(inputView, outputView, eventPlanningService);
+        controller.run();
     }
 }
