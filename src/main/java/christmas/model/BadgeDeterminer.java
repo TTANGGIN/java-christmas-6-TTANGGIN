f88d170d6efd22aa788136a8d24fc94caf3fbe62
TTANGGIN <58355254+TTANGGIN@users.noreply.github.com>
2023-11-12 15:28:14 +0900

    feat(BadgeDeterminer): 총혜택 금액에 따라 이벤트 배지를 부여한다.

---
+++ src/main/java/christmas/model/BadgeDeterminer.java
@@ -0,0 +1,39 @@
+package christmas.model;
+
+import christmas.config.EventBadge;
+
+public class BadgeDeterminer {
+
+    private final String eventBadge;
+
+    private BadgeDeterminer(String eventBadge) {
+        this.eventBadge = eventBadge;
+    }
+
+    public static BadgeDeterminer from(int totalDiscountAmount) {
+        return new BadgeDeterminer(determineEventBadge(totalDiscountAmount));
+    }
+
+    public static BadgeDeterminer empty() {
+        return new BadgeDeterminer(EventBadge.NONE.getBadge());
+    }
+
+    private static String determineEventBadge(int totalDiscountAmount) {
+        if (totalDiscountAmount >= EventBadge.SANTA.getAmount()) {
+            return EventBadge.SANTA.getBadge();
+        }
+        if (totalDiscountAmount >= EventBadge.TREE.getAmount()) {
+            return EventBadge.TREE.getBadge();
+        }
+        if (totalDiscountAmount >= EventBadge.STAR.getAmount()) {
+            return EventBadge.STAR.getBadge();
+        }
+
+        return EventBadge.NONE.getBadge();
+    }
+
+    @Override
+    public String toString() {
+        return eventBadge;
+    }
+}
